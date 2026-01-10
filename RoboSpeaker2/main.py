560b8206ed70bc8ac76b2f0ae4a06b733cacc534
sahusridhar23 <sahusridhar23@gmail.com>
2026-01-11 00:20:21 +0530

    issue solved: multiple lines of TTS now works

--- RoboSpeaker2/main.py
+++ RoboSpeaker2/main.py
@@ -1,14 +1,20 @@
 import pyttsx3
 
 print("WELCOME TO ROBOSPEAKER 1.1, cerated by sridhar sahu")
-engine = pyttsx3.init()
-engine.setProperty('rate',150)
 
 while True:
     text = input('Enter what you want me to speak:')
     if text.lower() == 'q':
+        engine = pyttsx3.init()
+        engine.setProperty('rate', 150)
         engine.say('bye bye friend')
         engine.runAndWait()
+        engine.stop()
+        del engine
         break
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 150)
     engine.say(text)
     engine.runAndWait()
+    engine.stop()
+    del engine
