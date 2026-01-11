d40fdef541417478d36dbe58f5906699ce7731cf
sahusridhar23 <sahusridhar23@gmail.com>
2026-01-11 08:48:38 +0530

    speaks temprature

--- weatherApp/main.py
+++ weatherApp/main.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import win32com.client as wincom
 
 print("Welcome to WeatherApp 1.0 .Created by Sridhar Sahu\n")
 
@@ -8,6 +9,8 @@ while True:
         city = input("Enter the city name:")
         if city.lower() == 'exit':
             print("Thank you for using WeatherApp 1.0")
+            speak = wincom.Dispatch("SAPI.SpVoice")
+            speak.speak("Thank you for using WeatherApp 1.0")
             break
         url = f"https://api.weatherapi.com/v1/current.json?key=d71e9e5a9b8345e8a4622336261101&q={city}"
         r = requests.get(url)
@@ -16,6 +19,8 @@ while True:
         wdic = json.loads(r.text) #parse json string to dictionary
         w = wdic["current"]["temp_c"]
         print('temp_C = ',w)
+        speak = wincom.Dispatch("SAPI.SpVoice")
+        speak.speak(f"Temprature in {city} is {w} degree celcius" )
     except:
         print("invalid city name, Please enter a valid city name")
 
